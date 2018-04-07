eb6572a452a4faa803cf5a68aff001e4b8ce6fc1
Cesar Augusto <caherdenez@gmail.com>
2018-04-07 10:35:21 -0500

    add signal ctrl c

--- sorteo.py
+++ sorteo.py
@@ -5,6 +5,8 @@
 # Licence:     BSD 3-Clause License
 #---------------------------------------------
 
+import signal
+import sys
 from random import shuffle
 
 test = ['Javier', 'Mateo', 'Leonardo', 'Oswal', 'Cesar', 'Ivan', 'Domicilio de Dominos']
@@ -14,6 +16,11 @@ def winner(data):
     shuffle(data)
     return data.pop()
 
+def exit_app(signal,frame):
+    print("\nFelicitaciones a todos los Ganadores\n\n")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, exit_app)
 while True:
     input("El Ganador es ....")
     print(winner(test))
